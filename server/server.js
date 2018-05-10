fbfa57658c9e7b1446addfe3b5dbe587d60cbb02
jordan boyer <jordan.boyer@epitech.eu>
2018-05-10 18:20:05 +0200

    add broadcast

--- server/server.js
+++ server/server.js
@@ -16,13 +16,30 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     socket.emit('newMessage', {
-        from: 'mike',
-        text: 'Hey. What is going on.',
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'new user joined',
         createdAt: new Date().getTime()
     });
 
     socket.on('createMessage', (msg) => {
         console.log('createMessage', msg);
+
+        io.emit('newMessage', {
+            from: msg.from,
+            text: msg.text,
+            createdAt: new Date().getTime()
+        });
+        /* socket.broadcast.emit('newMessage', {
+            from: msg.from,
+            text: msg.text,
+            createdAt: new Date().getTime()
+        }); */
     });
     
     socket.on('disconnect', () => {
