99420e392ed95b96765b3db41b14c2023db8adc3
jordan boyer <jordan.boyer@epitech.eu>
2018-05-11 15:58:49 +0200

    add geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -8,4 +8,13 @@ describe('generateMessage', () => {
         expect(res.text).toBe('easy');
         expect(typeof res.createdAt).toBe('number');
     });
-})
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        res = generateLocationMessage('admin', 1, 1);
+        expect(res.from).toBe('admin');
+        expect(res.url).toBe('https://www.google.com/maps?q=1,1');
+        expect(typeof res.createdAt).toBe('number');
+    });
+});
